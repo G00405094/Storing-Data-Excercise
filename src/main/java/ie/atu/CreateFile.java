e34ff1d3f7dd050ea1cab763baffee97f645cd12
G00405094 <G00405094@atu.ie>
2022-11-15 13:39:58 +0000

    Added writing to file

--- src/main/java/ie/atu/CreateFile.java
+++ src/main/java/ie/atu/CreateFile.java
@@ -1,13 +1,22 @@
 package ie.atu;
 
 import java.io.*;
+import java.nio.file.Path;
 
 public class CreateFile {
-    public static void main(String[] args){
-
+    public static void main(String[] args)
+    {
+        File myFile = new File("MyFile2.txt");
+        System.out.println("My file is located at " + myFile.getAbsolutePath());
+        String content = "Details to write";
+        try
         {
-            File myFile = new File("MyFile.txt");
-            System.out.println("My file is located at " + myFile.getAbsolutePath());
+            FileWriter myWriter = new FileWriter(myFile);
+            myWriter.write("gggg");
+            myWriter.close();
+        }catch (IOException e)
+        {
+            e.printStackTrace();
         }
 
     }
